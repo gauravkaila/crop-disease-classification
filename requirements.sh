f9949f3591520489c96510785d8ddedbde0933d3
Ubuntu <ubuntu@ip-172-31-31-228.ap-south-1.compute.internal>
2017-11-18 17:31:02 +0000

    dependancies requirements

---
+++ requirements.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#install pip
+sudo apt install python-pip
+
+# Install tensorflow
+pip install tensorflow
+
+# Install sklearn
+pip install -U scikit-learn
+
+# Install scipy
+pip install scipy
+
+# Install PIL
+pip install pillow
+
+# Install flask
+pip install flask
+
+# Install django
+pip install django
+
+# Install gunicorn
+pip install gunicorn
+
+# Install nginx
+sudo apt-get install nginx
